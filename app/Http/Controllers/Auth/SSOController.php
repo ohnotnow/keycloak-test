639ea0ef8a12fa04e57cd38b97686f3e1b4d038f
Ohffs <ohffsnotnow@gmail.com>
2025-07-03 13:55:44 +0100

    Faff

--- app/Http/Controllers/Auth/SSOController.php
+++ app/Http/Controllers/Auth/SSOController.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 
 class SSOController extends Controller
 {
-    public function redirectToProvider()
+    public function login()
     {
-        return Socialite::driver('keycloak')->with(['OAUTH2_PROXY_INSECURE_OIDC_ALLOW_UNVERIFIED_EMAIL' => true])->redirect();
+        if (config('sso.enabled', true)) {
+            return Socialite::driver('keycloak')->with(['OAUTH2_PROXY_INSECURE_OIDC_ALLOW_UNVERIFIED_EMAIL' => true])->redirect();
+        }
+
+        return view('auth.login');
+    }
+
+    public function doLocalLogin(Request $request)
+    {
+        if (config('sso.enabled', true)) {
+            abort(403, 'SSO is enabled');
+        }
+
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($request->only('username', 'password'))) {
+            return redirect()->intended('/home');
+        }
+
+        return redirect()->back()->withErrors(['username' => 'Invalid credentials']);
     }
 
     public function handleProviderCallback()
@@ -21,24 +44,41 @@ class SSOController extends Controller
 
             dump($ssoUser);
 
-            $user = User::updateOrCreate(
-                ['email' => strtolower($ssoUser->getEmail())],
-                [
-                    'password' => bcrypt(Str::random(64)),
-                    'username' => strtolower($ssoUser->getNickname() ?? $ssoUser->getName()),
-                    'email' => strtolower($ssoUser->getEmail()),
-                    'surname' => $this->extractSurname($ssoUser),
-                    'forenames' => $this->extractForenames($ssoUser),
-                    'is_staff' => $this->determineStaffStatus($ssoUser),
-                ]
-            );
+            if (!config('sso.allow_students', true) && $this->isStudent($ssoUser)) {
+                abort(403, 'Students are not allowed to login');
+            }
+
+            $email = strtolower($ssoUser->getEmail());
+
+            if (config('sso.autocreate_new_users', false)) {
+                $user = User::updateOrCreate(
+                    ['email' => $email],
+                    [
+                        'password' => bcrypt(Str::random(64)),
+                        'username' => strtolower($ssoUser->getNickname() ?? $ssoUser->getName()),
+                        'email' => strtolower($ssoUser->getEmail()),
+                        'surname' => $this->extractSurname($ssoUser),
+                        'forenames' => $this->extractForenames($ssoUser),
+                        'is_staff' => $this->isStaff($ssoUser),
+                    ]
+                );
+            } else {
+                $user = User::where('email', '=', $email)->firstOrFail();
+            }
+
+            if (config('sso.admins_only', false) && !$user->is_admin) {
+                if ($user->wasRecentlyCreated) {
+                    $user->delete();
+                }
+                abort(403, 'Only admins can login');
+            }
 
             auth()->login($user, true);
             return "Hello";
             return redirect('/home');
 
         } catch (\Exception $e) {
-            return redirect('/login')->with('error', 'SSO authentication failed');
+            abort(500, 'SSO authentication failed');
         }
     }
 
@@ -56,11 +96,18 @@ class SSOController extends Controller
                $this->parseFromName($ssoUser->getName(), 'forenames');
     }
 
-    private function determineStaffStatus($ssoUser)
+    private function isStudent($ssoUser)
     {
         $username = $ssoUser->getNickname() ?? $ssoUser->getName();
 
         return $this->looksLikeMatric($username);
+    }
+
+    private function isStaff($ssoUser)
+    {
+        $username = $ssoUser->getNickname() ?? $ssoUser->getName();
+
+        return !$this->looksLikeMatric($username);
     }
 
     private function looksLikeMatric($username)
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function() {
-    return redirect()->route('sso.redirect');
-})->name('login');
-
-Route::get('/auth/sso', [\App\Http\Controllers\Auth\SSOController::class, 'redirectToProvider'])->name('sso.redirect');
+Route::get('/login', [\App\Http\Controllers\Auth\SSOController::class, 'login'])->name('login');
+Route::post('/login', [\App\Http\Controllers\Auth\SSOController::class, 'doLocalLogin'])->name('login.do');
 Route::get('/auth/callback', [\App\Http\Controllers\Auth\SSOController::class, 'handleProviderCallback'])->name('sso.callback');
+
+// Route::get('/auth/sso', [\App\Http\Controllers\Auth\SSOController::class, 'redirectToProvider'])->name('sso.redirect');
 
 Route::get('/home', function () {
     dd(auth()->user());
 });
-
