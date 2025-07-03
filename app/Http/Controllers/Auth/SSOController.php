201dde1fa00b367ec957797a8ac5f253dad341f0
Ohffs <ohffsnotnow@gmail.com>
2025-07-03 11:11:57 +0100

    WIP

---
+++ app/Http/Controllers/Auth/SSOController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+class SSOController extends Controller
+{
+    public function redirectToProvider()
+    {
+        return Socialite::driver('keycloak')->with(['OAUTH2_PROXY_INSECURE_OIDC_ALLOW_UNVERIFIED_EMAIL' => true])->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        try {
+            $ssoUser = Socialite::driver('keycloak')->user();
+
+            $user = User::updateOrCreate(
+                ['email' => strtolower($ssoUser->getEmail())],
+                [
+                    'password' => bcrypt(Str::random(64)),
+                    'username' => strtolower($ssoUser->getNickname() ?? $ssoUser->getName()),
+                    'email' => strtolower($ssoUser->getEmail()),
+                    'surname' => $this->extractSurname($ssoUser),
+                    'forenames' => $this->extractForenames($ssoUser),
+                    'is_staff' => $this->determineStaffStatus($ssoUser),
+                ]
+            );
+
+            auth()->login($user, true);
+
+            return redirect('/home');
+
+        } catch (\Exception $e) {
+            return redirect('/login')->with('error', 'SSO authentication failed');
+        }
+    }
+
+    private function extractSurname($ssoUser)
+    {
+        return $ssoUser->user['family_name'] ??
+               $ssoUser->user['surname'] ??
+               $this->parseFromName($ssoUser->getName(), 'surname');
+    }
+
+    private function extractForenames($ssoUser)
+    {
+        return $ssoUser->user['given_name'] ??
+               $ssoUser->user['forenames'] ??
+               $this->parseFromName($ssoUser->getName(), 'forenames');
+    }
+
+    private function determineStaffStatus($ssoUser)
+    {
+        $username = $ssoUser->getNickname() ?? $ssoUser->getName();
+
+        return $this->looksLikeMatric($username);
+    }
+
+    private function looksLikeMatric($username)
+    {
+        return preg_match('/^[0-9]+[a-z]?$/', $username);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function() {
+    return redirect()->route('sso.redirect');
+})->name('login');
+
+Route::get('/auth/sso', [\App\Http\Controllers\Auth\SSOController::class, 'redirectToProvider'])->name('sso.redirect');
+Route::get('/auth/sso/callback', [\App\Http\Controllers\Auth\SSOController::class, 'handleProviderCallback'])->name('sso.callback');
+
+Route::get('/home', function () {
+    dd(auth()->user());
 });
+
