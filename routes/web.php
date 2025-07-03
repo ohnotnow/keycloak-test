f0525b5c88ab6c7924a2a8e6598295e286a6ba6c
Ohffs <ohffsnotnow@gmail.com>
2025-07-03 11:32:26 +0100

    Faff

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/login', function() {
 })->name('login');
 
 Route::get('/auth/sso', [\App\Http\Controllers\Auth\SSOController::class, 'redirectToProvider'])->name('sso.redirect');
-Route::get('/auth/sso/callback', [\App\Http\Controllers\Auth\SSOController::class, 'handleProviderCallback'])->name('sso.callback');
+Route::get('/auth/callback', [\App\Http\Controllers\Auth\SSOController::class, 'handleProviderCallback'])->name('sso.callback');
 
 Route::get('/home', function () {
     dd(auth()->user());
